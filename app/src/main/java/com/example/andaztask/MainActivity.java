c877ebad07ce5f74fcc1ad8e1a312478e18dad5b
unknown <rana.hamza2014@gmail.com>
2019-04-01 18:21:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/andaztask/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.andaztask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.back_arrow));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+             //   onBackPressed();
+            }
+        });
+    }
+}
