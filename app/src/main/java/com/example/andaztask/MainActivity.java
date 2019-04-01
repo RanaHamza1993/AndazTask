b1e6c3ed917fdc8530b327d754bb2bade6755390
unknown <rana.hamza2014@gmail.com>
2019-04-01 20:49:35 +0500

    push

--- app/src/main/java/com/example/andaztask/MainActivity.java
+++ app/src/main/java/com/example/andaztask/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.andaztask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextView alreadyAccount;
+    private TextView termsAndConditons;
+    String html="<font color=#000000>Already have an Account? </font> <a href><font color=#EE3F24>Log in</font><a/>";
+    String termshtml="<font color=#000000>Agree with our </font> <a href><font color=#EE3F24>terms &amp; conditions</font><a/>";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,10 @@ public class MainActivity extends AppCompatActivity {
              //   onBackPressed();
             }
         });
+        alreadyAccount=findViewById(R.id.already_account);
+        alreadyAccount.setText(Html.fromHtml(html));
+        termsAndConditons=findViewById(R.id.textView3);
+        termsAndConditons.setText(Html.fromHtml(termshtml));
+
     }
 }
