b56deabda0ef49c94be0e4b862bee0c8fcac9f32
unknown <rana.hamza2014@gmail.com>
2019-04-02 17:58:41 +0500

    push

--- app/src/main/java/com/example/andaztask/activities/Home.java
+++ app/src/main/java/com/example/andaztask/activities/Home.java
@@ -1,15 +1,60 @@
 package com.example.andaztask.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.andaztask.R;
+import com.example.andaztask.fragments.HomeFragment;
+import com.example.andaztask.fragments.MapFragment;
 
 public class Home extends AppCompatActivity {
 
+    ViewPager pager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        pager=findViewById(R.id.pager);
+        tabLayout=findViewById(R.id.tablayout);
+        PagerAdapter adapter=new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+
+                switch (i){
+                    case 0:{
+                        Fragment fragment=new HomeFragment();
+                        return fragment;
+                    }
+                    case 1:{
+                        Fragment fragment=new MapFragment();
+                        return fragment;
+                    }
+                    default:
+                        Fragment fragment=new HomeFragment();
+                        return fragment;
+
+
+                }
+
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_bottom_home);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_bottom_marker);
+
+        pager.setCurrentItem(0);
     }
 }
---
+++ app/src/main/java/com/example/andaztask/fragments/MapFragment.java
@@ -0,0 +1,57 @@
+package com.example.andaztask.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andaztask.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
+   GoogleMap mMap;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapFragment= (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment==null)
+        {
+            FragmentManager fm=getFragmentManager();
+            FragmentTransaction transaction= fm.beginTransaction();
+            mapFragment=SupportMapFragment.newInstance();
+            transaction.replace(R.id.map,mapFragment).commit();
+
+        }
+
+        mapFragment.getMapAsync(this);
+        return v;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
