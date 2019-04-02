faca23000a4416634da63439a835dae419c3dc91
unknown <rana.hamza2014@gmail.com>
2019-04-02 18:01:55 +0500

    push

--- app/src/main/java/com/example/andaztask/activities/MainActivity.java
+++ app/src/main/java/com/example/andaztask/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.andaztask.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.andaztask.R;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TextView alreadyAccount;
     private TextView termsAndConditons;
+    private Button register;
     String html="<font color=#000000>Already have an Account? </font> <a href><font color=#EE3F24>Log in</font><a/>";
     String termshtml="<font color=#000000>Agree with our </font> <a href><font color=#EE3F24>terms &amp; conditions</font><a/>";
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar=findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+        register=findViewById(R.id.appCompatButton);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.back_arrow));
@@ -38,5 +42,12 @@ public class MainActivity extends AppCompatActivity {
         termsAndConditons=findViewById(R.id.textView3);
         termsAndConditons.setText(Html.fromHtml(termshtml));
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
